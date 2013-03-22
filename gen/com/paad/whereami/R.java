bfb24e0b1f8807ebacbc1ec37f21b07ba8a6b2b6
Harrsh Ankola <harsh.ankola@gmail.com>
2013-03-22 20:12:48 +0530

    ver 1.2
    -Added: Mapview to show current location

--- gen/com/paad/whereami/R.java
+++ gen/com/paad/whereami/R.java
@@ -22,6 +22,7 @@ public final class R {
     public static final class id {
         public static final int LinearLayout1=0x7f070000;
         public static final int myLocationText=0x7f070001;
+        public static final int myMapView=0x7f070002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/com/paad/whereami/WhereAmI.java
+++ src/com/paad/whereami/WhereAmI.java
@@ -1,21 +1,48 @@
 package com.paad.whereami;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.TextView;
 
-public class WhereAmI extends Activity {
+public class WhereAmI extends MapActivity {
+
+	private MapController mapController;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+		// Get a reference to the MapView
+		MapView myMapView = (MapView) findViewById(R.id.myMapView);
+
+		// Get the Map View's controller
+		mapController = myMapView.getController();
+
+		// Configure the map display options
+		myMapView.setSatellite(true);
+		myMapView.setBuiltInZoomControls(true);
+
+		// Zoom in
+		mapController.setZoom(17);
 
 		String svcName = Context.LOCATION_SERVICE;
 		LocationManager locationManager = (LocationManager) getSystemService(svcName);
@@ -46,13 +73,51 @@ public class WhereAmI extends Activity {
 		myLocationText = (TextView) findViewById(R.id.myLocationText);
 
 		String latLongString = "No location found";
+		String addressString = "No address found";
 		if (location != null) {
+			// Update the map location.
+			Double geoLat = location.getLatitude() * 1E6;
+			Double geoLng = location.getLongitude() * 1E6;
+			GeoPoint point = new GeoPoint(geoLat.intValue(), geoLng.intValue());
+			mapController.animateTo(point);
+
 			double lat = location.getLatitude();
 			double lng = location.getLongitude();
 			latLongString = "Lat:" + lat + "\nLong:" + lng;
+
+			double latitude = location.getLatitude();
+			double longitude = location.getLongitude();
+
+			Geocoder gc = new Geocoder(this, Locale.getDefault());
+
+			if (!Geocoder.isPresent()) {
+				addressString = "No geocoder available";
+			} else {
+				try {
+					List<Address> addresses = gc.getFromLocation(latitude,
+							longitude, 1);
+					StringBuilder sb = new StringBuilder();
+					if (addresses.size() > 0) {
+						Address address = addresses.get(0);
+
+						for (int i = 0; i < address.getMaxAddressLineIndex(); i++) {
+							sb.append(address.getAddressLine(i)).append("\n");
+						}
+
+						sb.append(address.getLocality()).append("\n");
+						sb.append(address.getPostalCode()).append("\n");
+						sb.append(address.getCountryName());
+					}
+					addressString = sb.toString();
+				} catch (IOException e) {
+					// TODO: handle exception
+					Log.d("WHEREAMI", "IO Exception", e);
+				}
+			}
 		}
 
-		myLocationText.setText("Your Current Position is:\n" + latLongString);
+		myLocationText.setText("Your Current Position is:\n" + latLongString
+				+ "\n\n" + addressString);
 	}
 
 	private final LocationListener locationListener = new LocationListener() {
@@ -81,4 +146,15 @@ public class WhereAmI extends Activity {
 			updateWithNewLocation(location);
 		}
 	};
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.google.android.maps.MapActivity#isRouteDisplayed()
+	 */
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
